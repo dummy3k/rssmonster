6cd3095b59ef2ef2410b49a2a667d6b163e38364
dummy3k <l4711@gmx.net>
2009-10-17 23:17:50 +0200

    prettyfied routing

--- rssmonster/config/routing.py
+++ rssmonster/config/routing.py
@@ -24,6 +24,8 @@ def make_map():
 
     map.connect('/add', controller='feed', action='add')
     map.connect('/list', controller='feed', action='show_list')
+    map.connect('/feed/{id}', controller='feed', action='show_feed')
+    map.connect('/signout', controller='login', action='signout')
 
     map.connect('/{controller}/{action}')
     map.connect('/{controller}/{action}/')
