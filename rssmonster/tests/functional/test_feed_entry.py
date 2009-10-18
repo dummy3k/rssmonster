a56415ba3c953af1982ea48247d71e38e97e25ec
dummy3k <l4711@gmx.net>
2009-10-18 21:21:10 +0200

    Revert "paster controller feed_entry"
    This reverts commit 61d8ee0e0cb186b3fc14d8aaac3ae33a20e8f0e4.

--- rssmonster/tests/functional/test_feed_entry.py
+++
@@ -1,7 +0,0 @@
-from rssmonster.tests import *
-
-class TestFeedEntryController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='feed_entry', action='index'))
-        # Test response...
