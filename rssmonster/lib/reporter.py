9da04b8618d1f7a018d84ba3a99139008db3c682
dummy3k <l4711@gmx.net>
2009-11-14 14:58:21 +0100

    more complex tests, before entry queue

--- rssmonster/lib/reporter.py
+++ rssmonster/lib/reporter.py
@@ -2,21 +2,34 @@ import logging
 log = logging.getLogger(__name__)
 
 class Reporter():
-    def __init__(self, feed, last_report, delta, add_ham):
+    def __init__(self, feed, last_report, last_last_report, delta, add_ham,
+                report_spam, max_entries = 30):
+
         self.feed = feed
         self.delta = delta
         self.last_report = last_report
+        self.last_last_report = last_last_report
         self.spam_entries = []
-        self.add_ham = add_ham  
-        
+        self.add_ham = add_ham
+        self.report_spam = report_spam
+        self.offset_id = None
+
     def add_item_bak(self, title, link, description, is_spam,
                  author_email=None, author_name=None, author_link=None,
                  pubdate=None, comments=None, unique_id=None,
                  enclosure=None, categories=(), item_copyright=None,
                  ttl=None):
         pass
-        
+
     def add_item(self, entry, is_spam):
+        if not self.last_report:
+            self.last_report = entry.updated
+
+        if entry.updated > self.last_report + self.delta:
+            self.report_spam()
+            self.last_last_report = self.last_report
+            self.last_report = entry.updated
+
         if not is_spam:
             #~ self.feed.add_item(title="title", link="link",
                 #~ description="description", pubdate=entry.updated)
@@ -26,5 +39,4 @@ class Reporter():
         if entry.updated < self.last_report + self.delta:
             self.spam_entries.append(entry)
             return
-            
-        
+
--- rssmonster/tests/test_reporter.py
+++ rssmonster/tests/test_reporter.py
@@ -1,6 +1,6 @@
 from rssmonster.tests import *
 from rssmonster.lib.reporter import Reporter
-from mock import Mock 
+from mock import Mock
 from datetime import datetime, timedelta
 
 import logging
@@ -10,7 +10,7 @@ def create_mock(values):
     retval = Mock()
     for k in values:
         retval.__dict__[k] = values[k]
-        
+
     return retval
 
 class TestLearningMocking(TestController):
@@ -22,17 +22,17 @@ class TestLearningMocking(TestController):
 class TestReporter(TestController):
     def __init__(self, foo):
         TestController.__init__(self, foo)
-        
+
         log.debug("--- running %s" % foo)
 
     def setUp(self):
-        self.reporter = Reporter(Mock(), datetime(2009,11,14, 12,00), 
-            timedelta(minutes=5), Mock())
-        
+        self.reporter = Reporter(Mock(), datetime(2009,11,14, 12,00),
+            None, timedelta(minutes=5), Mock(), Mock())
+
     def test_add_ham(self):
         self.reporter.add_item(Mock(), False)
         self.assertTrue(self.reporter.add_ham.called)
-        
+
         #~ self.reporter.add_item(title="Sample post",
                    #~ link="", description="",
                    #~ is_spam=False,
@@ -40,7 +40,7 @@ class TestReporter(TestController):
         #~ f.add_item.assert_called_with(title="Sample post",
                    #~ link="", description="",
                    #~ pubdate=datetime(2009,11,14, 12,00))
-        #~ 
+        #~
         #~ f.reset_mock()
         #~ self.assertFalse(self.reporter.add_ham.called)
 
@@ -53,14 +53,78 @@ class TestReporter(TestController):
         self.assertNotEqual(Mock(), self.reporter.spam_entries[0])
         self.assertEqual(1, len(self.reporter.spam_entries))
 
-    def test_suppress2(self):
+    def test_report_spam(self):
         entry = create_mock({'updated':datetime(2009,11,14, 12,01)})
         self.reporter.add_item(entry, True)
         self.assertFalse(self.reporter.add_ham.called)
         self.assertEqual(entry, self.reporter.spam_entries[0])
 
         entry = create_mock({'updated':datetime(2009,11,14, 12,07)})
-        self.reporter.add_item(Mock(), False)
+        self.reporter.add_item(entry, False)
         self.assertTrue(self.reporter.add_ham.called)
+        self.assertTrue(self.reporter.report_spam.called)
+        self.assertEqual(datetime(2009,11,14, 12,07),
+            self.reporter.last_report)
 
-        #self.assertTrue(r.add_ham.called)
+class TestReporterInitial(TestController):
+
+
+    def test_reality(self):
+        self.reporter = Reporter(Mock(), None, None, timedelta(minutes=5), Mock(), Mock())
+        self.assertEqual(None, self.reporter.last_last_report)
+
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,01)}), True)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,02)}), True)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,03)}), True)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,04)}), True)
+        self.assertFalse(self.reporter.report_spam.called)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,11)}), True)
+        #   report
+        self.assertTrue(self.reporter.report_spam.called)
+        self.assertEqual(datetime(2009,11,14, 12,11),
+            self.reporter.last_report)
+        self.assertEqual(datetime(2009,11,14, 12,01),
+            self.reporter.last_last_report)
+        #~ self.assertEqual(None, self.reporter.last_last_report)
+
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,12)}), True)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,12)}), True)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,22)}), False)
+        #   report
+        self.assertEqual(datetime(2009,11,14, 12,22),
+            self.reporter.last_report)
+        self.assertEqual(datetime(2009,11,14, 12,11),
+            self.reporter.last_last_report)
+
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,13)}), True)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,13)}), True)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,33)}), True)
+        #   report
+        self.assertEqual(datetime(2009,11,14, 12,33),
+            self.reporter.last_report)
+        self.assertEqual(datetime(2009,11,14, 12,22),
+            self.reporter.last_last_report)
+
+    def test_reality_from_step2(self):
+        self.reporter = Reporter(Mock(), datetime(2009,11,14, 12,11),
+            datetime(2009,11,14, 12,01), timedelta(minutes=5), Mock(), Mock())
+
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,12)}), True)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,12)}), True)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,22)}), False)
+        #   report
+        self.assertEqual(datetime(2009,11,14, 12,22),
+            self.reporter.last_report)
+        self.assertEqual(datetime(2009,11,14, 12,11),
+            self.reporter.last_last_report)
+
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,13)}), True)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,13)}), True)
+        self.reporter.add_item(create_mock({'updated':datetime(2009,11,14, 12,33)}), True)
+        #   report
+        self.assertEqual(datetime(2009,11,14, 12,33),
+            self.reporter.last_report)
+        self.assertEqual(datetime(2009,11,14, 12,22),
+            self.reporter.last_last_report)
+
+
